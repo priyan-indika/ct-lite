b8b5d5c0c4032a55f3de8fdfe245f7efda436aba
indika <iindika@gmail.com>
2025-11-21 00:09:16 +1300

    Added In-Memory DB support

--- PractTest/PractTest.API/Program.cs
+++ PractTest/PractTest.API/Program.cs
@@ -8,6 +8,7 @@ using PractTest.Application;
 using PractTest.Application.DTOValidations;
 using PractTest.Application.Interfaces.Customers;
 using PractTest.Application.Services;
+using PractTest.Domain.Entities;
 using PractTest.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -28,12 +29,33 @@ builder.Services.AddSingleton(mapper);
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+//builder.Services.AddDbContext<AppDbContext>(options =>
+//    options.UseInMemoryDatabase("TestDb"));
+
 builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
 builder.Services.AddScoped<ICustomerService, CustomerService>();
 
 builder.Services.AddValidatorsFromAssemblyContaining<CreateCustomerValidator>();
 
 var app = builder.Build();
+
+//using (var scope = app.Services.CreateScope())
+//{
+//    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+//    if (!context.Customers.Any())
+//    {
+//        context.Customers.AddRange(
+//            new Customer { Id = 1, Username = "aaaa", Password = "AAAA", Name = "Aaaa", Email = "a@a.com", LoyaltyPoints = 11, CreatedBy = 0 },
+//            new Customer { Id = 2, Username = "bbbb", Password = "BBBB", Name = "Bbbb", Email = "b@a.com", LoyaltyPoints = 25, CreatedBy = 0 },
+//            new Customer { Id = 3, Username = "cccc", Password = "CCCC", Name = "Cccc", Email = "c@a.com", LoyaltyPoints = 45, CreatedBy = 0 },
+//            new Customer { Id = 4, Username = "dddd", Password = "DDDD", Name = "Dddd", Email = "d@a.com", LoyaltyPoints = 74, CreatedBy = 0 },
+//            new Customer { Id = 5, Username = "eeee", Password = "EEEE", Name = "Eeee", Email = "e@a.com", LoyaltyPoints = 0, CreatedBy = 0 }
+//        );
+
+//        context.SaveChanges();
+//    }
+//}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
